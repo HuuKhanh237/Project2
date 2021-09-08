601055ada33f10aca99d6c7a34d68f289d122f85
HuuKhanh237 <vuongkhanh40@gmail.com>
2021-09-08 14:14:25 +0700

    ss

--- project2/resources/views/technicians/layouts/mau2.blade.php
+++ project2/resources/views/technicians/layouts/mau2.blade.php
@@ -13,76 +13,71 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
     <style type="text/css">
-body{
-      width: 100%;
-      background: url("storage/kaka1.jpg");
-      color: white;
-    }
-</style>
+        body {
+            width: 100%;
+            background: url("storage/kaka1.jpg");
+            color: white;
+        }
+    </style>
 </head>
 
-<body >
+<body>
     <!-- Site wrapper -->
-    
+
     <div><img src="storage/ll6.gif" /></div>
     <div class="wrapper" style="width: 100%;">
         <!-- Navbar -->
-        
-    
+
+
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
             <!-- Left navbar links -->
-            <div name="header" >
-            
-            <ul class="navbar-nav" >
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="#" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="#" class="nav-link">Contact</a>
-                </li>
-            </ul>
+            <div name="header">
 
-            <!-- Right navbar links -->
-            <ul class="navbar-nav" style="float : left">
-                <!-- Navbar Search -->
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                        <i class="fas fa-search"></i>
-                    </a>
-                    <div class="navbar-search-block">
-                        <form class="form-inline">
-                            <div class="input-group input-group-sm">
-                                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                                <div class="input-group-append">
-                                    <button class="btn btn-navbar" type="submit">
-                                        <i class="fas fa-search"></i>
-                                    </button>
-                                    <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                        <i class="fas fa-times"></i>
-                                    </button>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                    </li>
+                    <li class="nav-item d-none d-sm-inline-block">
+                        <a href="#" class="nav-link"><i style="font-size: 20px;" class="fas fa-home"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                            <i class="fas fa-search"></i>
+                        </a>
+                        <div class="navbar-search-block">
+                            <form class="form-inline">
+                                <div class="input-group input-group-sm">
+                                    <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-navbar" type="submit">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                        <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                                            <i class="fas fa-times"></i>
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
-                    </div>
-                </li>
+                            </form>
+                        </div>
+                    </li>
 
-                <!-- Messages Dropdown Menu -->
+                    <!-- Messages Dropdown Menu -->
 
 
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                        <i class="fas fa-expand-arrows-alt"></i>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                        <i class="fas fa-th-large"></i>
-                    </a>
-                </li>
-            </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                            <i class="fas fa-expand-arrows-alt"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+                            <i class="fas fa-th-large"></i>
+                        </a>
+                    </li>
+                </ul>
+
+                <!-- Right navbar links -->
+                
         </nav>
         <!-- /.navbar -->
 
@@ -99,11 +94,11 @@ body{
                 <!-- Sidebar user (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image" style="font-size: 20px; color: white; margin-top : 5px">
-                    <i class="fas fa-user-tie"></i>
-        </div>
+                        <i class="fas fa-user-tie"></i>
+                    </div>
                     <div class="info" style="font-size: 20px;">
                         <a href="#" class="d-block">
-                        @section('tren')
+                            @section('tren')
                             @if(Auth::guard('user')->user() != null)
                             {{Auth::guard('user')->user()->name}}
                             @endif
@@ -169,24 +164,24 @@ body{
                                     </a>
                                 </li>
                             </ul>
-</li>
-<li class="nav-item">
-            <a href="../widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Kho
-               
-              </p>
-            </a>
-          </li>
-</ul>
-                           
+                        </li>
+                        <li class="nav-item">
+                            <a href="../widgets.html" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    Kho
+
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+
 
 
 
                 </nav>
                 <div style=" padding-right: 5px;">
-                <a href="{{route('logout')}}"><button type="button" class="btn btn-outline-danger" style="float: right"><i class="fas fa-sign-out-alt" style="font-size: 30px;"></i></button></a>
+                    <a href="{{route('logout')}}"><button type="button" class="btn btn-outline-danger" style="float: right"><i class="fas fa-sign-out-alt" style="font-size: 30px;"></i></button></a>
                 </div>
                 <!-- /.sidebar-menu -->
             </div>
@@ -194,21 +189,24 @@ body{
         </aside>
 
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
+        <div class="content-wrapper" style="color: black;">
             <!-- Content Header (Page header) -->
 
 
             <!-- Main content -->
-            <section class="content">
+            <!-- <section class="content">
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-12">
                             <!-- Default box -->
-
+            <!-- 
                         </div>
                     </div>
                 </div>
-            </section>
+            </section>  -->
+            <div class="content">
+                @yield('content')
+            </div>
             <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
@@ -223,7 +221,7 @@ body{
             <!-- Control sidebar content goes here -->
         </aside>
         <!-- /.control-sidebar -->
-        
+
     </div>
     <!-- ./wrapper -->
 
